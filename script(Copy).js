b06c7a33d3074dd52ea3a5aa880a81f0fc13c4bb
codex2814 <114537636+codex2814@users.noreply.github.com>
2024-02-25 23:16:50 +0100

    Add files via upload

---
+++ script(Copy).js
@@ -0,0 +1,32 @@
+const download = document.querySelector('.download-btn');
+const countdown = document.querySelector('.countdown');
+const pleaseWaitText = document.querySelector('.pleaseWait-text');
+const manualDownloadText = document.querySelector('.manualDownload-text');
+const manualDownloadLink = document.querySelector('.manualDownload-link');
+var timeLeft = 15;
+
+download.addEventListener('click', () => {
+  download.style.display = "none";
+  countdown.innerHTML = `Download will begin automatically in <span>${timeLeft}</span> seconds`;
+  
+  var downloadTimer = setInterval(function timeCount() {
+    timeLeft--;
+    countdown.innerHTML = `Download will begin automatically in <span>${timeLeft}</span> seconds`;
+    
+    if (timeLeft <= 0) {
+      clearInterval(downloadTimer);
+
+      pleaseWaitText.style.display = "block";
+      let download_href = "Buy Tomorrow Today. Etben.pdf";
+
+      window.location.href = download_href;
+      manualDownloadLink.href = download_href; 
+
+
+      setTimeout(() => {
+        pleaseWaitText.style.display = "none";
+        manualDownloadText.style.display = "block";
+      }, 1000);
+    }
+  }, 1000);
+});
